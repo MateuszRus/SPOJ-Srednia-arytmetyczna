d5e9d5a2a74db0e2dcc9753b836381678dc0585c
Mateusz <mateusz@mrsolutions.pl>
2018-11-07 14:08:25 +0100

    Add project files.

---
+++ Srednia Arytmetyczna/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Srednia_Arytmetyczna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
